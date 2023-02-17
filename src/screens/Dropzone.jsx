edb2a1f932452e3763fde3fa397988c16f595b4b
shubham pal <shubhampalatwork@gmail.com>
2023-02-18 01:50:45 +0530

    final updates

--- src/screens/Dropzone.jsx
+++ src/screens/Dropzone.jsx
@@ -3,18 +3,20 @@ import "./Dropzone.scss";
 import { useDropzone } from 'react-dropzone';
 import { Image } from 'cloudinary-react';
 import axios from 'axios';
-import { CopyToClipboard } from "react-copy-to-clipboard"
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { ThreeCircles } from "react-loader-spinner";
 
-const CLOUDINARY_UPLOAD_PRESET = 'htrt9fqi';
-const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/doh91aq3h/image/upload';
+const CLOUDINARY_UPLOAD_PRESET = process.env.CLOUDINARY_UPLOAD_PRESET;
+const CLOUDINARY_UPLOAD_URL = process.env.CLOUDINARY_UPLOAD_URL;
 
 export function MyDropzone() {
-  const [images, setImages] = useState([{url : "https://res.cloudinary.com/doh91aq3h/image/upload/v1676578133/logo-final.png.png"}]);
-  // const [imageURL, setImageURL] = useState([]);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: 'image/*',
     multiple: true,
     onDrop: useCallback((acceptedFiles) => {
+      setLoading(!loading);
       acceptedFiles.forEach((file) => {
         const formData = new FormData();
         formData.append('file', file);
@@ -24,18 +26,31 @@ export function MyDropzone() {
         axios.post(CLOUDINARY_UPLOAD_URL, formData, {
           headers: { 'X-Requested-With': 'XMLHttpRequest' },
         }).then((response) => {
+          setLoading(false);
           setImages((prevImages) => [...prevImages, { url: response.data.secure_url }]);
-          // setImageURL((prevURLs) => [...prevURLs, response.data.secure_url ]);
         }).catch((error) => {
           console.log(error);
         });
       });
     }, [])
+    
   });
-  
+
 
   return (
     <div className='main_container'>
+      <ThreeCircles
+        height="100"
+        width="100"
+        color="#4fa94d"
+        wrapperStyle={{}}
+        wrapperClass="loading"
+        visible={loading}
+        ariaLabel="three-circles-rotating"
+        outerCircleColor=""
+        innerCircleColor=""
+        middleCircleColor=""
+      />
       <div className='dropzone_'>
         <div className='dropzone__container' {...getRootProps()}>
           <input {...getInputProps()} />
@@ -50,8 +65,8 @@ export function MyDropzone() {
           </div>
         </div>
         <ul className='dropzone__url'>
-          {images.map((image) => (
-            <li key={image.url} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          {images.map((image, i) => (
+            <li key={image.url+i} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
               <Image className="image_" cloudName="doh91aq3h" publicId={image.url} width="50" />
               <input className='input' type="text" readOnly value={image.url} />
               <CopyToClipboard
